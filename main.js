d25a3ffe349970fbf6b8e2f43cb979d65a81b36f
Macbook Pro <macbookpro@Macbook-pro-2.local>
2024-06-01 01:21:59 +0500

    Updated

--- main.js
+++ main.js
@@ -13,6 +13,8 @@ dotenv.config()
 const token = '7068981084:AAH0GZcT6-E70Q5GOX-2iZHV9bPzy3Q_iwQ'
 const bot = new TelegramBot(token, { polling: true });
 
+
+
 bot.setMyCommands([
     {
         command: "/start",
@@ -22,8 +24,8 @@ bot.setMyCommands([
         command: "/info",
         description: "Bot haqida malumot olish"
     }, {
-        command: "/foydalanuvchilar",
-        description: "Userlarni ko'rish"
+        command: "/userlar_soni",
+        description: "User sonini ko'rish"
     }
 ]);
 
@@ -45,9 +47,9 @@ const button = {
             ],
         ]
     }
-}
+};
 
-const contactbtn = {
+const contactbtns = {
     reply_markup: {
         keyboard: [
             [
@@ -65,22 +67,30 @@ const contactbtn = {
 const btns = {
     reply_markup: {
         keyboard: [
+            
             [
                 {
-                    text: 'Locatsiyani yuborish',
-                    request_location: true
-                },
+                    text: "Test"
+                }
             ],
+        ],
+        resize_keyboard: true
+    }
+};
+
+export const contactbtn = {
+    reply_markup: {
+        keyboard: [
             [
                 {
-                    text: 'Contactni yuborish',
+                    text: "contact",
                     request_contact: true
                 },
-            ],
+            ]
         ],
         one_time_keyboard: true
     }
-};
+}
 
 
 
@@ -89,51 +99,42 @@ connectMongodb()
 const start = async () => {
     bot.on('message', async (msg) => {
         const chatId = msg.chat.id;
+        const messageId = msg.message_id
         const text = msg.text;
         const first_name = msg.chat.first_name;
         const username = msg.chat.username;
         const userType = msg.chat.type;
-
+        const last_name = msg.chat.last_name
         const contact = msg.contact;
         const location = msg.location;
-
-        const abduvohid = 5352461835
-
+       
+        
         const user = await findByChatId(chatId);
         if (!user) {
             const data = await register(chatId, first_name, username, userType);
         };
 
-        if (text === '/koruser') {
+        if (text === '/userlar_soni') {
             const AllUser = await findAll();
-            console.log(AllUser);
-
-            for (let i = 0; i < AllUser.length; i++) {
-                bot.sendMessage(chatId,
-                    `${i + 1}. ChatId: ${AllUser[i].chatId}
-Firstname: ${AllUser[i].first_name}
-Username: ${AllUser[i].username}`);
-            };
-            return 0
+            return bot.sendMessage(chatId, AllUser.length);
         };
 
-
-        if (text && text.split(' ')[0] === 'a') {
-            bot.sendMessage(chatId, text);
-            return bot.sendMessage(chatId, text);
-        }
-
+        if (text === '/start') {
+            return bot.sendMessage(chatId, `Assalomu alaykum ${username}  👋
+Botimizga xush kelibsiz 🎉
+Kontaktingizni  📱  yuboring! (Yuborish uchun tugma ⬇️)`, contactbtn);
+          }
         if (text) {
             bot.sendMessage(chatId, text, btns);
+            bot.deleteMessage(chatId, messageId);
         };
+
 
     });
 
     bot.on('contact', (msg) => {
         const chatId = msg.chat.id;
         const contact = msg.contact;
-
-        console.log(contact);
         bot.sendMessage(chatId, contact.phone_number)
     })
 
@@ -147,17 +148,17 @@ Username: ${AllUser[i].username}`);
     });
 
 
-    bot.on('callback_query', async (msg) => {
-        const chatId = msg.from.id
+    // bot.on('callback_query', async (msg) => {
+    //     const chatId = msg.from.id
 
-        const AllUser = await findAll();
-        for (let i = 0; i < AllUser.length; i++) {
-            bot.sendMessage(chatId,
-                `${i + 1}. ChatId: ${AllUser[i].chatId}\n
-            firstname: ${AllUser[i].first_name}\n
-            Username: ${AllUser[i].username}`);
-        };
-    });
+    //     const AllUser = await findAll();
+    //     for (let i = 0; i < AllUser.length; i++) {
+    //         bot.sendMessage(chatId,
+    //             `${i + 1}. ChatId: ${AllUser[i].chatId}\n
+    //         firstname: ${AllUser[i].first_name}\n
+    //         Username: ${AllUser[i].username}`);
+    //     };
+    // });
 };
 
 
